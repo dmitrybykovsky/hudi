db734caa3bb070322f230e199f5651539132892d
M172 <m172@AdminHBs-MacBook-Pro.local>
2018-11-11 12:55:18 +0400

    Added docu

--- docker/setup_demo_postgres.sh
+++ docker/setup_demo_postgres.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./setup_demo.sh
+./setup_demo.sh compose/postgres-kafka-hdfs.yml
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @compose/register-postgres.json
