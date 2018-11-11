e222c47f09724b63dddc76b0c456f3bae52ce6df
M172 <m172@AdminHBs-MacBook-Pro.local>
2018-11-11 10:16:20 +0400

    Demo init commit

---
+++ docker/setup_demo_postgres.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./setup_demo.sh
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @compose/register-postgres.json
