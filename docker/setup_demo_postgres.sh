f660a2b4f9c58fca32d994bc8df1bba8750dca91
Anmol Gautam <anmol.gautam@honestbee.com>
2018-11-19 11:39:11 +0800

    added debezium version=0.8

--- docker/setup_demo_postgres.sh
+++ docker/setup_demo_postgres.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
+export DEBEZIUM_VERSION=0.8
 ./setup_demo.sh compose/postgres-kafka-hdfs.yml
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @compose/register-postgres.json
