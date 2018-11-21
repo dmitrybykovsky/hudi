e16652dfc23df7a841c537d508d561732fd9a068
Dmitry Bykovsky <m172@AdminHBs-MacBook-Pro.local>
2018-11-21 19:31:44 +0800

    Implemented delete support

--- docker/setup_demo_postgres.sh
+++ docker/setup_demo_postgres.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 export DEBEZIUM_VERSION=0.8
+export MY_HUDI_EXTRA_VERSION=1.1-SNAPSHOT
+
+rm -rf /tmp/my-kafka-transforms.jar
+curl -fSL -o /tmp/my-kafka-transforms.jar \
+https://github.com/dmitrybykovsky/kafkatransforms/releases/download/${MY_HUDI_EXTRA_VERSION}/kafka-transforms-${MY_HUDI_EXTRA_VERSION}.jar
+cp /tmp/my-kafka-transforms.jar hoodie/hadoop/hive_base/target/
 
 ./setup_demo.sh compose/postgres-kafka-hdfs.yml
 
